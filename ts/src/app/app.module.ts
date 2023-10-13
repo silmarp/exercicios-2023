51c21b859b2dcf72b94d98e84a463d8c18596d03
Silmar <silmarjr2@gmail.com>
2023-10-13 10:32:49 -0300

    create sideBar component

--- ts/src/app/app.module.ts
+++ ts/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideBarComponent
   ],
   imports: [
     BrowserModule,
