7378bebe6d99145423cd7c58e94f04d4fcc2f5b0
Silmar <silmarjr2@gmail.com>
2023-10-19 14:57:12 -0300

    add form import

--- ts/src/app/shared/modules/material/material.module.ts
+++ ts/src/app/shared/modules/material/material.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 const materialModules = [
   MatIconModule,
   MatSliderModule,
   MatCardModule,
+  MatFormFieldModule,
 ];
 
 @NgModule({
