96b44933be221a4b79b785747646048a83392111
Silmar <silmarjr2@gmail.com>
2023-10-19 13:31:41 -0300

    create discussion and topic components

--- ts/src/app/app.module.ts
+++ ts/src/app/app.module.ts
@@ -10,6 +10,8 @@ import { ContentComponent } from './content/content.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './shared/modules/material/material.module';
 import { FooterComponent } from './footer/footer.component';
+import { DiscussionComponent } from './discussion/discussion.component';
+import { TopicComponent } from './discussion/topic/topic.component';
 
 @NgModule({
   declarations: [
@@ -18,6 +20,8 @@ import { FooterComponent } from './footer/footer.component';
     HeaderComponent,
     ContentComponent,
     FooterComponent,
+    DiscussionComponent,
+    TopicComponent,
   ],
   imports: [
     BrowserModule,
