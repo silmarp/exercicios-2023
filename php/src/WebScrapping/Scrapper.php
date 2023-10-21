7fe3bbe9c3d9cc0b473f46aaf37c1e713e8aca7d
Silmar <silmarjr2@gmail.com>
2023-10-21 20:18:09 -0300

    create xpath filter

--- php/src/WebScrapping/Scrapper.php
+++ php/src/WebScrapping/Scrapper.php
@@ -4,6 +4,7 @@ namespace Chuva\Php\WebScrapping;
 
 use Chuva\Php\WebScrapping\Entity\Paper;
 use Chuva\Php\WebScrapping\Entity\Person;
+use DOMXPath;
 
 /**
  * Does the scrapping of a webpage.
@@ -14,6 +15,13 @@ class Scrapper {
    * Loads paper information from the HTML and returns the array with the data.
    */
   public function scrap(\DOMDocument $dom): array {
+    // Filter a elements with paper-card class with xPath
+    $paperCards = (new DOMXPath($dom))->query("//a[contains(@class, 'paper-card')]"); 
+
+    foreach ($paperCards as $paperCard) {
+        print_r($paperCard);
+    }
+
     return [
       new Paper(
         123,
@@ -26,5 +34,4 @@ class Scrapper {
       ),
     ];
   }
-
 }
