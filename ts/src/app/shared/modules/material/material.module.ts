cd9de45620f3dd8ad486e530911dce67ba36d96a
Silmar <silmarjr2@gmail.com>
2023-10-22 14:57:17 -0300

    update footer css

--- ts/src/app/shared/modules/material/material.module.ts
+++ ts/src/app/shared/modules/material/material.module.ts
@@ -6,7 +6,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
-
+import { MatDividerModule } from '@angular/material/divider';
 const materialModules = [
   MatIconModule,
   MatSliderModule,
@@ -14,6 +14,7 @@ const materialModules = [
   MatFormFieldModule,
   MatInputModule,
   MatButtonModule,
+  MatDividerModule,
 ];
 
 @NgModule({
