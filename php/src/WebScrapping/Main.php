c426da7849e69c807eaafdba817e1b388cd22f5c
Silmar <silmarjr2@gmail.com>
2023-10-21 22:00:31 -0300

    create exporting to xlsx

--- php/src/WebScrapping/Main.php
+++ php/src/WebScrapping/Main.php
@@ -2,6 +2,9 @@
 
 namespace Chuva\Php\WebScrapping;
 
+use Box\Spout\Writer\Common\Creator\WriterEntityFactory;
+use Box\Spout\Common\Entity\Row;
+use OpenSpout\Writer\Common\Creator\WriterEntityFactory as OpenSpoutWriterEntityFactory;
 /**
  * Runner for the Webscrapping exercice.
  */
@@ -17,6 +20,37 @@ class Main {
     $data = (new Scrapper())->scrap($dom);
 
     // Write your logic to save the output file bellow.
+    $writer = OpenSpoutWriterEntityFactory::createXLSXWriter();
+    $writer->openToFile('./output.xlsx');
+
+    /*
+    $cells = [
+        OpenSpoutWriterEntityFactory::createCell('Carl'),
+        OpenSpoutWriterEntityFactory::createCell('is not'),
+        OpenSpoutWriterEntityFactory::createCell('great!'),
+    ];
+
+    $singleRow = OpenSpoutWriterEntityFactory::createRow($cells);
+    $writer->addRow($singleRow);   
+
+    */
+
+    foreach($data as $paper) {
+        $cells = [
+            OpenSpoutWriterEntityFactory::createCell($paper->id),
+            OpenSpoutWriterEntityFactory::createCell($paper->title),
+            OpenSpoutWriterEntityFactory::createCell($paper->type),
+        ];
+        foreach($paper->authors as $key=>$author){
+            array_push($cells, OpenSpoutWriterEntityFactory::createCell($author->name));
+            array_push($cells, OpenSpoutWriterEntityFactory::createCell($author->institution));
+        }
+        
+        $singleRow = OpenSpoutWriterEntityFactory::createRow($cells);
+        $writer->addRow($singleRow);   
+    }
+
+    $writer->close();
     print_r($data);
   }
 
